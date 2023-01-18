13b4865f8702ad36b320c5a0dc026820a2c5a731
Zeinab-kr <zeinabk654@gmail.com>
2023-01-19 03:12:49 +0330

    Server preparation

---
+++ src/Server/Server.java
@@ -0,0 +1,15 @@
+package Server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
+/**
+ * This class implements the server socket
+ *
+ */
+public class Server {
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(5757);
+
+    }
+}
